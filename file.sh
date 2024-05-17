d1c15071a45b25b6558a13bade6be9360db872a5
VANGA NAVEEN REDDY <116937218+naveenreddy939@users.noreply.github.com>
2024-05-17 15:21:59 +0530

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define directory and file names
+DIR="output"
+FILE="output.txt"
+
+# Create directory if it doesn't exist
+mkdir -p $DIR
+
+# Create a text file with some content
+echo "This is a sample file created on $(date)" > $DIR/$FILE
